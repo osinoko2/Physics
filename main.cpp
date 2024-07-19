69d02281ea8b0c66c4c1465acd4805e6b367883e
osinoko <k023g0114@g.neec.ac.jp>
2024-07-19 14:43:33 +0900

    円錐振り子を作ってみよう

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 const char kWindowTitle[] = "LE2B_12_サクライショウセイ_タイトル";
+
+struct Vector3 {
+	float x, y, z;
+};
+
+struct ConicalPendulum
+{
+	Vector3 anchor; // アンカーポイント。固定された端の位置
+	float length; // 紐の長さ
+	float halfApexAngle; // 円錐の直角の半分
+	float angle; // 現在の角度
+	float angularVelocity; // 角速度
+};
+
+struct Ball {
+	Vector3 position; // ボールの位置
+	Vector3 velocity; // ボールの速度
+	Vector3 acceleration; // ボールの加速度
+	float mass; // ボールの質量
+	float radius; // ボールの半径
+	unsigned int color; // ボールの色
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +35,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+	Ball ball;
+	ConicalPendulum conicalPendulum;
+	conicalPendulum.anchor = { 0.0f, 1.0f, 0.0f };
+	conicalPendulum.length = 0.8f;
+	conicalPendulum.halfApexAngle = 0.7f;
+	conicalPendulum.angle = 0.0f;
+	conicalPendulum.angularVelocity = 0.0f;
+	float deltaTime = 1.0f / 60.0f;
+	float radius = std::sin(conicalPendulum.halfApexAngle) * conicalPendulum.length;
+	float height = std::cos(conicalPendulum.halfApexAngle) * conicalPendulum.length;
+	ball.position.x = conicalPendulum.anchor.x + std::cos(conicalPendulum.angle) * radius;
+	ball.position.y = conicalPendulum.anchor.y - height;
+	ball.position.z = conicalPendulum.anchor.z - std::sin(conicalPendulum.angle) * radius;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,6 +61,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+
+		conicalPendulum.angularVelocity = std::sqrt(9.8f / (conicalPendulum.length * std::cos(conicalPendulum.halfApexAngle)));
+		conicalPendulum.angle += conicalPendulum.angularVelocity * deltaTime;
 
 		///
 		/// ↑更新処理ここまで
